78f9505cfbb680f81578f7dc124ca3f88028d7b1
prasanth-periyasamy <prasanth.periyasamy@numentica-ui.com>
2024-03-22 10:20:11 +0530

    Services page and Contact us page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,2 +1,104 @@
-const ContactUs = () => {};
+import styles from "./contact.module.css";
+interface Content {
+  id: number;
+  imgUrl: string;
+  title: string;
+  addressData: {
+    title?: string;
+    address?: string;
+    location?: string;
+    phoneNumber?: string;
+    mail?: string;
+  }[];
+}
+
+const content: Content[] = [
+  {
+    id: 1,
+    imgUrl: "",
+    title: "Address",
+    addressData: [
+      {
+        title: "TFC_1",
+        address: "2/498 Chennai main road",
+        location: "chennai-600001",
+      },
+      {
+        title: "TFC_1",
+        address: "2/498 Chennai main road",
+        location: "chennai-600001",
+      },
+      {
+        title: "TFC_1",
+        address: "2/498 Chennai main road",
+        location: "chennai-600001",
+      },
+    ],
+  },
+  {
+    id: 2,
+    imgUrl: "",
+    title: "Phone",
+    addressData: [
+      {
+        title: "TFC_1",
+        phoneNumber: "1234567890",
+      },
+      {
+        title: "TFC_1",
+        phoneNumber: "1234567890",
+      },
+    ],
+  },
+  {
+    id: 3,
+    imgUrl: "",
+    title: "Email",
+    addressData: [
+      {
+        title: "TFC_1",
+        mail: "tcf1.org",
+      },
+      {
+        title: "TFC_1",
+        mail: "tcf1.org",
+      },
+    ],
+  },
+];
+
+const ContactUs = () => {
+  return (
+    <main className={styles.contactus_main_container}>
+      <header className="text_center">
+        <h1 className="header_name">Contact us</h1>
+      </header>
+      <section className={styles.contactus_section}>
+        {content.map((data) => (
+          <div className={styles.contactus_item} key={data?.id} >
+            <div className={styles.contactus_item_icon}>
+              <img src={data?.imgUrl} alt={data?.title} />
+            </div>
+            <h1 className={styles.contact_head}>{data?.title}</h1>
+            {data?.addressData?.map((item) => (
+              <div className={styles.location_list}>
+                <div>
+                  <h3 className={styles.location_head}>{item?.title}</h3>
+                  {data.title === "Address" && (
+                    <div>
+                      <p>{item.address}</p>
+                      <p>{item.location}</p>
+                    </div>
+                  )}
+                  {data.title === "Phone" && <p>{item.phoneNumber}</p>}
+                  {data.title === "Email" && <p>{item.mail}</p>}
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </section>
+    </main>
+  );
+};
 export default ContactUs;
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,2 +1,54 @@
-const Service = () => {};
+import styles from "./service.module.css";
+interface ServiceData {
+  id:number;
+  title: string;
+  content: string;
+  imgUrl: string;
+}
+
+const serviceData: ServiceData[] = [
+  { 
+    id:1,
+    title: "TeleDoctor Facilitation Center",
+    content:
+      "TeleDoctor Facilitation Centers are designed to bridge the gap between virtual and in-person healthcare, offering a unique space where patients can access a variety of healthcare services supported by TeleDoctor's digital platform",
+    imgUrl: "../Images/center.svg",
+  },
+  {
+    id:2,
+    title: "Get Prescriptions online",
+    content:
+      " WithTeleDoctor, securing your prescriptions has never been easier.From the comfort of your home, and at any time that suits you,  gain access to the medical prescriptions you need. Our streamlined process ensures that you receive your medication quickly and hassle-free, directly through our platform.",
+    imgUrl: "../Images/new.svg",
+  },
+  {
+    id:3,
+    title: "Get Lab Requisitions",
+    content:
+      "TeleDoctor simplifies the process of obtaining lab requisitions,ensuring you can access necessary laboratory tests without the  traditional hassle. Here's how our platform assists you in accessing the necessary lab tests.",
+    imgUrl: "../Images/prescription.svg",
+  },
+];
+const Service = () => {
+  return (
+    <div className={styles.service_container}>
+      <main>
+        <header className="text_center">
+          <h1 className="header_name">Our Services</h1>
+        </header>
+        <section className={styles.services}>
+          {serviceData?.map((data) => (
+            <article className={`${styles.services_list}`} key={data?.id} >
+              <div className={styles.service_img_container}>
+                <img src={data?.imgUrl} alt={data?.title} />
+              </div>
+              <h3 className="text_center">{data?.title}</h3>
+              <p>{data?.content}</p>
+            </article>
+          ))}
+        </section>
+      </main>
+    </div>
+  );
+};
 export default Service;
